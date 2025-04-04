b713c679c3c0d7720fa4cc755a4b2489ca9bc7a7
佐々木誠ニコラス <sasakimakotonikorasu0809@gmail.com>
2025-04-05 00:01:28 +0900

    fix: load Firebase key from env

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 // server/db.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./onomatoparty2-firebase-adminsdk-fbsvc-b05605cf5e.json");
+
+// 環境変数から秘密鍵をパース
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
